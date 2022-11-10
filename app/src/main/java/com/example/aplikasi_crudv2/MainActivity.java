92f526d37ccc4f22435ae8b16ca945508b6cb7fd
Benhard Sim <36811298+BenhardSim@users.noreply.github.com>
2022-11-10 17:26:14 +0700

    add db

--- app/src/main/java/com/example/aplikasi_crudv2/MainActivity.java
+++ app/src/main/java/com/example/aplikasi_crudv2/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.aplikasi_crudv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    DataHelper dbcenter;
+    protected Cursor cursor;
+    public MainActivity ma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
