8d159740e2154c7676d320d9e1f70df98ec6be9a
Julius Adrian <juliusa368@gmail.com>
2022-11-10 17:18:24 +0700

    Menu

--- app/src/main/java/com/example/aplikasi_crudv2/FormActivity.java
+++ app/src/main/java/com/example/aplikasi_crudv2/FormActivity.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class FormActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_form);
+        Button btnSubmit = findViewById(R.id.btn_submit);
+        btnSubmit.setOnClickListener(this);
     }
 
     @Override
@@ -29,12 +33,21 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
-                Intent intentList = new Intent(MainActivity.this, MainActivity.class);
+                Intent intentList = new Intent(FormActivity.this, MainActivity.class);
                 startActivity(intentList);
                 break;
             case R.id.action_form:
-                Intent intentForm = new Intent(MainActivity.this, FormActivity.class);
+                Intent intentForm = new Intent(FormActivity.this, FormActivity.class);
                 startActivity(intentForm);
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_submit:
+                // kode untuk insert ke database
                 break;
         }
     }
