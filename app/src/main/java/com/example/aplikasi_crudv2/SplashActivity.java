3d7af6acafdca280bb3a944dc8a3cdc0c86fa993
Julius Adrian <juliusa368@gmail.com>
2022-11-11 16:32:52 +0700

    Added fix for SplashActivity

---
+++ app/src/main/java/com/example/aplikasi_crudv2/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.aplikasi_crudv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler mHandler = new Handler();
+        Runnable mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        // its trigger runnable after 3000 millisecond.
+        mHandler.postDelayed(mRunnable,3000);
+    }
+}
